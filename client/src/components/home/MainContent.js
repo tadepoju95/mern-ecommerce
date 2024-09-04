3f3ed855e614bc396fb6ce13079d4d74def27378
tadepoju95 <tadepoju95@gmail.com>
2024-09-03 20:12:30 -0500

    Modified files in project folder.

--- client/src/components/home/MainContent.js
+++ client/src/components/home/MainContent.js
@@ -9,9 +9,9 @@ const MainContent = () => {
 	return (
 			<div className="hero">
 			<div className="hero-text">
-				<p className="info">NEW PRODUCT</p>
-				<h1 className="hi">XX99 MARK II HEADPHONES</h1>
-				<p className="bye">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+				<p className="hero-text-header">NEW PRODUCT</p>
+				<h1 className="hero-header">XX99 MARK II HEADPHONES</h1>
+				<p className="hero-text-info">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
 				<button className="hero-button" type="button">SEE PRODUCT</button> 
 			</div>
 			</div>
